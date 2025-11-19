7e4eb419127bb6e6a2abdee8cee14597b3a33d72
CHIN-C <pvchinmayi@gmail.com>
2025-11-19 19:08:12 +0530

    New Lab001 added

---
+++ src/ex_01_Java_Basic/Lab004_ControlStatements.java
@@ -0,0 +1,27 @@
+package ex_01_Java_Basic;
+
+public class Lab004_ControlStatements {
+    public static void main(String args[] )
+    {
+        int a = 500;
+        int b = 800;
+
+        if(a<b)
+
+            System.out.println("b is a bigger no");
+
+        else if (a>b)
+
+            System.out.println("a is a bigger no");
+
+        else if (a==b)
+
+        System.out.println("a is equal b");
+
+        else
+
+        System.out.println("a is not equal to b");
+
+    }
+
+}
