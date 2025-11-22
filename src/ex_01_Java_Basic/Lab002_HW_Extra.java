f0af47ca4a6457300865d12bff63ecab7b177681
CHIN-C <pvchinmayi@gmail.com>
2025-11-22 14:50:05 +0530

    New Lab002

---
+++ src/ex_01_Java_Basic/Lab002_HW_Extra.java
@@ -0,0 +1,34 @@
+package ex_01_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Lab002_HW_Extra {
+    public static void main(String[] args) {
+        int x= 100 ;
+        int y= 50 ;
+        int z = (x+y);
+        boolean Apple = true;
+        boolean Orange = false;
+        String Computerone = "This is an old System";
+        String Computertwo = "This is an oldest System";
+        String Computerthree = "This is a New System";
+        char xyz = '1' , abc = '8';
+        float F = 0.0234566f;
+        double D = 2.00000000000000000d;
+        char myVar1 = 65 , myVar2 = 68, myVar3 = 67;
+        System.out.println(F);
+        System.out.println(D);
+        System.out.println(myVar1);
+        System.out.println(myVar3);
+
+        System.out.println(xyz);
+
+        System.out.println(z);
+        System.out.println(abc);
+        System.out.println(Apple);
+        System.out.println(Computerone);
+        System.out.println(Computertwo);
+        System.out.println(Computerthree);
+
+    }
+}
